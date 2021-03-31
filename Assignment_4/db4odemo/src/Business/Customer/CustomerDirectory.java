384f1e21ddd88c0e063ef2c083e7f4d6144960f2
joshi-pa <joshi.pa@northeastern.edu>
2021-03-30 21:38:44 -0400

    Adding Customer, Restaurant Changes

--- Assignment_4/db4odemo/src/Business/Customer/CustomerDirectory.java
+++ Assignment_4/db4odemo/src/Business/Customer/CustomerDirectory.java
@@ -5,10 +5,46 @@
  */
 package Business.Customer;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author harold
+ * @author 12013
  */
 public class CustomerDirectory {
+    private ArrayList<Customer> customerDirectory;
     
+    public CustomerDirectory() {
+        
+        customerDirectory = new ArrayList();
+        
+    }
+
+    public ArrayList<Customer> getCustomerDirectory() {
+        return customerDirectory;
+    }
+
+    public void setCustomerDirectory(ArrayList<Customer> customerDirectory) {
+        this.customerDirectory = customerDirectory;
+    }
+    
+    public Customer newCustomer(String name, String email, String phoneNumber, int age, String homeAddress, String userName){
+        Customer customer = new Customer(name, email, phoneNumber, age, homeAddress, userName);
+        customerDirectory.add(customer);
+        return customer;
+    }
+    
+    public void removeCustomer(Customer customer){
+        customerDirectory.remove(customer);
+    }
+    
+    public Customer getCustomer(String name){
+        for(Customer customer: customerDirectory){
+            if(customer.getName().equalsIgnoreCase(name)){
+                return customer;
+            }
+        }
+        return null;
+    }
+
 }
--- Assignment_4/db4odemo/src/Business/Restaurant/Restaurant.java
+++ Assignment_4/db4odemo/src/Business/Restaurant/Restaurant.java
@@ -11,4 +11,77 @@ package Business.Restaurant;
  */
 public class Restaurant {
     
+    private String restaurantName;
+    private String address;
+    private String managerName;
+    private String phoneNumber;
+    private MenuDirectory MenuDirectory;
+    private String restRating;
+    
+    public Restaurant(String restaurantName, String address, String managerName, String phoneNumber, String restRating) {
+        this.restaurantName = restaurantName;
+        this.address = address;
+        this.managerName = managerName;
+        this.phoneNumber = phoneNumber;
+        this.restRating = restRating;
+    }
+
+    public MenuDirectory getMenuDirectory() {
+        return MenuDirectory;
+    }
+
+    public void setMenuDirectory(MenuDirectory MenuDirectory) {
+        this.MenuDirectory = MenuDirectory;
+    }
+    
+    
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRestRating() {
+        return restRating;
+    }
+
+    public void setRestRating(String restRating) {
+        this.restRating = restRating;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return restaurantName;
+    }
+
+
+    
 }
